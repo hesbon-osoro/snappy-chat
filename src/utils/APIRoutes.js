09f6bf6c0442b109f2565d00790e62d53ff85a52
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-28 23:54:43 +0300

    server host url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,6 +1,5 @@
 // Server url
-export const host =
-	'http://localhost:5000' || 'https://snappy-server.cyclic.app/';
+export const host = process.env.SERVER_HOST
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
